2d8cd3842a79db02c8eff6c763be47b59af254f9
mohan <mohan@DESKTOP-ETIN4UD>
2018-01-18 21:42:38 +0800

    ReaderWriteStateMonad

---
+++ src/main/scala/example/RWSExample.scala
@@ -0,0 +1,34 @@
+package example
+
+import scalaz._
+import Scalaz._
+
+object RWSExample extends App {
+  case class Config(port: Int)
+
+  def log[R, S](msg: String): ReaderWriterState[R, List[String], S, Unit] =
+    ReaderWriterStateT {
+      case (r, s) => (msg.format(r, s) :: Nil, (), s).point[Id]
+    }
+
+  def invokeService: ReaderWriterState[Config, List[String], Int, Int] =
+    ReaderWriterStateT {
+      case (cfg, invocationCount) => (
+        List("Invoking service with port " + cfg.port),
+        scala.util.Random.nextInt(100),
+        invocationCount + 1).point[Id]
+    }
+
+  val program: RWS[Config, List[String], Int, Int] = for {
+    _ <- log("Start - r: %s, s: %s")
+    res <- invokeService
+    _ <- log("Between - r: %s, s: %s")
+    _ <- invokeService
+    _ <- log("Done - r: %s, s: %s")
+  } yield res
+
+  val (logMessages, result, invocationCount) = program run (Config(443), 0)
+  println("Result: " + result)
+  println("Service invocations: " + invocationCount)
+  println("Log: %n%s".format(logMessages.mkString("\t", "%n\t".format(), "")))
+}
